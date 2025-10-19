7277d4c7a0ab1765396826758132767d5127c0bb
azurei <endziopalkij@gmail.com>
2025-10-19 14:30:54 +0200

    updating for Necesse's 1.0

--- src/main/java/soulchasm/main/misc/ExoticMerchantMethodPatch.java
+++ src/main/java/soulchasm/main/misc/ExoticMerchantMethodPatch.java
@@ -6,14 +6,16 @@ import necesse.entity.mobs.friendly.human.humanShop.ExoticMerchantHumanMob;
 import necesse.entity.mobs.friendly.human.humanShop.SellingShopItem;
 import net.bytebuddy.asm.Advice;
 
-@ModConstructorPatch(target = ExoticMerchantHumanMob.class, arguments = {})
+@ModConstructorPatch(
+        target = ExoticMerchantHumanMob.class,
+        arguments = {}
+)
 public class ExoticMerchantMethodPatch {
-
     @Advice.OnMethodExit
     static void onExit(@Advice.This ExoticMerchantHumanMob merchantMob) {
-        String[] plushies = new String[]{"argemiaplushieitem", "v1plushieitem", "fairplushieitem", "fumoplushieitem", "devplushieitem"};
+        String[] plushies = new String[]{"argemiaplushieitem", "v1plushieitem", "fairplushieitem", "fumoplushieitem", "devplushieitem", "sharkplushieitem"};
         GameRandom random = new GameRandom();
-        merchantMob.shop.addSellingItem("plushie", new SellingShopItem(2, 2)).setItem(random.getOneOf(plushies)).setRandomPrice(100, 200);
+        merchantMob.shop.addSellingItem("plushie", new SellingShopItem(5, 2)).setItem(random.getOneOf(plushies)).setRandomPrice(100, 200);
         merchantMob.shop.addSellingItem("tobeblindfold", new SellingShopItem()).setRandomPrice(100, 200);
     }
 }
